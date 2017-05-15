19eb524db2793ae5f83d24b2cbb2efef23b12d29
Thomas Wiltshire <thomas.wiltshire14@icloud.com>
2017-05-15 16:34:32 +1000

    Small changes.

--- Container/DynamicArray.h
+++ Container/DynamicArray.h
@@ -1,8 +1,43 @@
 #pragma once
+
+template<typename T>
 class DynamicArray
 {
 public:
-	DynamicArray();
+
+	// constructor
+	DynamicArray()
+	{
+	}
+
+	// copy constructor 
+	DynamicArray(const DynamicArray& other)
+	{
+	
+	}
+	
+	// distructor  
 	~DynamicArray();
+	{
+	
+	}
+
+	// assignment operator
+	DynamicArray& operator= (const DynamicArray& other)
+	{
+	
+	}
+
+	// Create the array
+	void CreateArray(int m_nSize)
+	{
+		m_date = new T[m_size];
+		m_capacity = m_nSize;
+		m_used = 0;
+	}
+
+	T* m_data;
+	int m_nCapacity;
+	int m_nUsed;
 };
 
--- Container/LinkedLists.h
+++ Container/LinkedLists.h
@@ -1,8 +1,19 @@
 #pragma once
+#include "ListNode.h"
+
+template<typename T>
 class LinkedLists
 {
 public:
+
 	LinkedLists();
 	~LinkedLists();
-};
 
+	void PushBack(T data)
+	{
+		
+	}
+
+	ListNode<T>* start;
+	ListNode<T>* end;
+};
