7512a4d4162e888637cfebe048c7661cca887820
Thomas Wiltshire <thomas.wiltshire14@icloud.com>
2017-06-25 01:37:56 +1000

    Final change before submitting.

--- Container/DynamicArray.h
+++ Container/DynamicArray.h
@@ -2,12 +2,20 @@
 #pragma once
 #include <memory.h>
 
-
+//--------------------------------------------------------------------------------------
+// DynamicArray Templated object.
+//--------------------------------------------------------------------------------------
 template<typename T>
 class DynamicArray
 {
 public:
 
+	//--------------------------------------------------------------------------------------
+	// Constructor taking in an Int.
+	//
+	// Param:
+	//		initialSize: the starting size of the array.
+	//--------------------------------------------------------------------------------------
 	DynamicArray(int initialSize = 0)
 	{
 		int nCapacity = initialSize;
@@ -21,12 +29,18 @@ public:
 
 		memset(&m_NullValue, 0, sizeof(T));
 	}
-	
+
+	//--------------------------------------------------------------------------------------
+	// Default Destructor
+	//--------------------------------------------------------------------------------------
 	~DynamicArray()
 	{
 		delete m_pData;
 	}
 
+	//--------------------------------------------------------------------------------------
+	// Default Constructor.
+	//--------------------------------------------------------------------------------------
 	DynamicArray(const DynamicArray& other)
 	{
 		m_nCapacity = other.m_nCapacity;
@@ -35,7 +49,13 @@ public:
 		m_pData = new T[other.m_nCapacity];
 		memccpy(m_pData, other.m_pData, sizeof(T) * other.m_nCapacity);
 	}
-	
+
+	//--------------------------------------------------------------------------------------
+	// PushBack: Push a value onto the back
+	//
+	// Param:
+	//		value: the value you are pushing on.
+	//--------------------------------------------------------------------------------------
 	void PushBack(T value)
 	{
 		if (m_nUsed >= m_nCapacity)
@@ -45,17 +65,28 @@ public:
 		++m_nUsed;
 	}
 
+	//--------------------------------------------------------------------------------------
+	// PushFront: Push a value onto the front
+	//
+	// Param:
+	//		value: the value you are pushing on.
+	//--------------------------------------------------------------------------------------
 	void PushFront(T value)
 	{
 		Insert(0, value);
 	}
 
+	//--------------------------------------------------------------------------------------
+	// Insert: Insert a value at an index.
+	//
+	// Param:
+	//		index: where you want to place the value.
+	//		value: the value you want to place in the array.
+	//--------------------------------------------------------------------------------------
 	void Insert(int index, T value)
 	{
-		if (m_nUsed > m_nCapacity)
+		if (m_nUsed >= m_nCapacity)
 			Resize();
-
-		//memcpy(m_pData + index + 1, m_pData + index, sizeof(T) * (m_nUsed - index));
 
 		for (int i = m_nUsed - 1; i >= index; --i)
 		{
@@ -66,12 +97,27 @@ public:
 		++m_nUsed;
 	}
 
+	//--------------------------------------------------------------------------------------
+	// PopBack: Pop a value off the array.
+	//
+	// Return:
+	//		T: returns T
+	//--------------------------------------------------------------------------------------
 	T PopBack()
 	{
 		--m_nUsed;
 		return m_pData[m_nUsed];
 	}
 
+	//--------------------------------------------------------------------------------------
+	// Remove: Remove a value from an index.
+	//
+	// Param:
+	//		index: where to remove the value from
+	//
+	// Return:
+	//		T: return T.
+	//--------------------------------------------------------------------------------------
 	T Remove(int index)
 	{
 		// Backup value that we are removing from array
@@ -89,16 +135,31 @@ public:
 		return value;
 	}
 
+	//--------------------------------------------------------------------------------------
+	// PopFront: Pop a value off the front.
+	//
+	// Return:
+	//		T: Returns T
+	//--------------------------------------------------------------------------------------
 	T PopFront()
 	{
 		return Remove(0);
 	}
 
-	T Clear()
+	//--------------------------------------------------------------------------------------
+	// Clear: clear the array.
+	//--------------------------------------------------------------------------------------
+	void Clear()
 	{
 		m_nUsed = 0;
 	}
 
+	//--------------------------------------------------------------------------------------
+	// Shrink: Shirk the array.
+	//
+	// return:
+	//		T: returns T
+	//--------------------------------------------------------------------------------------
 	T Shrink()
 	{
 		int nCapacity = m_nUsed;
@@ -108,23 +169,42 @@ public:
 
 		T* newData = new T[nCapacity];
 		memcpy(newData, m_pData, sizeof(T) * nCapacity);
-		delete m_pData
+		delete m_pData;
 		m_pData = newData;
 		m_nCapacity = nCapacity;
 	}
 
+	//--------------------------------------------------------------------------------------
+	// Sub-script operator returning a reference
+	//
+	// Param:
+	//		index: takes in an int.
+	//
+	// Return:
+	//		T: Returns a type T
+	//--------------------------------------------------------------------------------------
 	T& operator[](const int index)
 	{
-
-
 		return m_pData[index];
 	}
 
+	//--------------------------------------------------------------------------------------
+	// Size: Return the size of the array.
+	//
+	// Return:
+	//		int: returns an int of the array size.
+	//--------------------------------------------------------------------------------------
 	int Size()
 	{
 		return m_nUsed;
 	}
 
+	//--------------------------------------------------------------------------------------
+	// Capacity: returns the capacity of the array.
+	//
+	// Return:
+	//		int: returns an int of the array capacity.
+	//--------------------------------------------------------------------------------------
 	int Capacity()
 	{
 		return m_nCapacity;
@@ -132,10 +212,16 @@ public:
 
 private:
 
+	//--------------------------------------------------------------------------------------
+	// Copy constructor etc
+	//--------------------------------------------------------------------------------------
 	DynamicArray& operator= (const DynamicArray& other) = default;
 	DynamicArray(DynamicArray&& other) = default;
 	DynamicArray& operator= (DynamicArray&& other) = default;
 
+	//--------------------------------------------------------------------------------------
+	// Resize: Resize the array.
+	//--------------------------------------------------------------------------------------
 	void Resize()
 	{
 		// Creates new array that is twice as big
@@ -150,12 +236,27 @@ private:
 		// Make sure our pointers still work
 		m_pData = newData;
 
+		// Increase capacity
 		m_nCapacity = m_nCapacity * 2;
 	}
 
+	//--------------------------------------------------------------------------------------
+	// a T pointer m_pData.
+	//--------------------------------------------------------------------------------------
 	T* m_pData;
-	int m_nCapacity;
-	int m_nUsed;
-	T m_NullValue
-};
 
+	//--------------------------------------------------------------------------------------
+	// An int m_nCapacity
+	//--------------------------------------------------------------------------------------
+	int m_nCapacity;
+
+	//--------------------------------------------------------------------------------------
+	// An int m_nUsed
+	//--------------------------------------------------------------------------------------
+	int m_nUsed;
+
+	//--------------------------------------------------------------------------------------
+	// a T m_NullValue
+	//--------------------------------------------------------------------------------------
+	T m_NullValue;
+};
--- Container/Source.cpp
+++ Container/Source.cpp
@@ -1,48 +1,199 @@
 #include <iostream>
 using namespace std;
 #include "DynamicArray.h"
+#include "Stack.h"
+#include "AssociativeArray.h"
 
 int main()
 {
-	// add all of them for testing. organize better. Seperate into different functions for each test.
+	// DYNAMICARRAY
 	DynamicArray<int> myArray(3);
 	
-	cout << "--Size--" << endl;
+	cout << "//////// TEST DYNAMIC ARRAY ////////" << endl;
+
+	cout << "---- SIZE ----" << endl;
 	cout << "Used: " << myArray.Size() << endl;
-	cout << "Capacity: " << myArray.Capacity() << endl << endl;
-	
-	cout << "--Adding Size--" << endl;
+	cout << "Capacity: " << myArray.Capacity() << endl;
+	cout << "---- SIZE ----" << endl << endl;
+
+	cout << "---- PUSHBACK ----" << endl;
 	myArray.PushBack(9);
 	myArray.PushBack(8);
-	myArray.PushBack(7);
-	myArray.PushBack(6);
-	myArray.PushBack(5);
-
 	cout << "Used: " << myArray.Size() << endl;
-	cout << "Capacity: " << myArray.Capacity() << endl << endl;
-
-	cout << "Values are" << endl;
+	cout << "Capacity: " << myArray.Capacity() << endl;
+	cout << "Values are:" << endl;
 	for (int i = 0; i < myArray.Size(); ++i)
 	{
 		cout << myArray[i] << endl;
 	}
+	cout << "---- PUSHBACK ----" << endl << endl;
 
+	cout << "---- PUSHFRONT ----" << endl;
+	myArray.PushFront(6);
+	myArray.PushFront(7);
+	cout << "Used: " << myArray.Size() << endl;
+	cout << "Capacity: " << myArray.Capacity() << endl;
+	cout << "Values are:" << endl;
+	for (int i = 0; i < myArray.Size(); ++i)
+	{
+		cout << myArray[i] << endl;
+	}
+	cout << "---- PUSHFRONT ----" << endl << endl;
+
+	cout << "---- INSERT ----" << endl;
 	myArray.Insert(1, 4);
-
-	cout << "Values are" << endl;
+	cout << "Used: " << myArray.Size() << endl;
+	cout << "Capacity: " << myArray.Capacity() << endl;
+	cout << "Values are:" << endl;
 	for (int i = 0; i < myArray.Size(); ++i)
 	{
 		cout << myArray[i] << endl;
 	}
+	cout << "---- INSERT ----" << endl << endl;
 
+	cout << "---- POPBACK ----" << endl;
 	int value = myArray.PopBack();
-	cout << "Values Popped " << value << endl;
-
-	cout << "Values are" << endl;
+	cout << "Values Popped:" << value << endl;
+	cout << "Used: " << myArray.Size() << endl;
+	cout << "Capacity: " << myArray.Capacity() << endl;
+	cout << "Values are:" << endl;
 	for (int i = 0; i < myArray.Size(); ++i)
 	{
 		cout << myArray[i] << endl;
 	}
+	cout << "---- POPBACK ----" << endl << endl;
+
+	cout << "---- REMOVE ----" << endl;
+	myArray.Remove(2);
+	cout << "Used: " << myArray.Size() << endl;
+	cout << "Capacity: " << myArray.Capacity() << endl;
+	cout << "Values are:" << endl;
+	for (int i = 0; i < myArray.Size(); ++i)
+	{
+		cout << myArray[i] << endl;
+	}
+	cout << "---- REMOVE ----" << endl << endl;
+
+	cout << "---- POPFRONT ----" << endl;
+	myArray.PopFront();
+	cout << "Used: " << myArray.Size() << endl;
+	cout << "Capacity: " << myArray.Capacity() << endl;
+	cout << "Values are:" << endl;
+	for (int i = 0; i < myArray.Size(); ++i)
+	{
+		cout << myArray[i] << endl;
+	}
+	cout << "---- POPFRONT ----" << endl << endl;
+
+	cout << "---- CLEAR ----" << endl;
+	myArray.Clear();
+	cout << "Used: " << myArray.Size() << endl;
+	cout << "Capacity: " << myArray.Capacity() << endl;
+	cout << "Values are:" << endl;
+	for (int i = 0; i < myArray.Size(); ++i)
+	{
+		cout << myArray[i] << endl;
+	}
+	cout << "---- CLEAR ----" << endl;
+
+	cout << "//////// TEST DYNAMIC ARRAY ////////" << endl << endl << endl << endl;
+
+	// MAP
+	AssociativeArray<int> myAssociativeArray;
+
+	cout << "//////// TEST ASSOCIATIVE ARRAY ////////" << endl;
+
+	cout << "---- ADDITEM ----" << endl;
+	myAssociativeArray.AddItem("Name1", 0);
+	cout << "Size: " << myAssociativeArray.Size() << endl;
+	cout << "Values are:" << endl;
+	for (int i = 0; i < myAssociativeArray.Size(); ++i)
+	{
+		cout << myAssociativeArray[i] << endl;
+	}
+	cout << "---- ADDITEM ----" << endl << endl;
+
+	cout << "---- GETITEMNAME ----" << endl;
+	char* text = myAssociativeArray.GetItemName(0);
+	cout << "Size: " << myAssociativeArray.Size() << endl;
+	cout << "Values are:" << endl;
+	for (int i = 0; i < myAssociativeArray.Size(); ++i)
+	{
+		cout << text << endl;
+	}
+	cout << "---- GETITEMNAME ----" << endl << endl;
+
+	cout << "---- ISITEM ----" << endl;
+	bool isitem = myAssociativeArray.IsItem("Name1");
+	cout << "Size: " << myAssociativeArray.Size() << endl;
+	cout << "Is Name1 an item:" << endl;
+	
+	if (isitem)
+	{
+		cout << "true" << endl;
+	}
+	else
+	{
+		cout << "false" << endl;
+	}
+	cout << "---- ISITEM ----" << endl << endl;
+
+	cout << "---- CLEAR ----" << endl;
+	myAssociativeArray.Clear();
+	cout << "Size: " << myAssociativeArray.Size() << endl;
+	cout << "Values are:" << endl;
+	for (int i = 0; i < myAssociativeArray.Size(); ++i)
+	{
+		cout << myAssociativeArray[i] << endl;
+	}
+	cout << "---- CLEAR ----" << endl << endl;
+
+	cout << "//////// TEST ASSOCIATIVE ARRAY ////////" << endl << endl << endl << endl;
+
+	// STACK
+	Stack<int> myStack;
+
+	cout << "//////// TEST STACK ////////" << endl;
+
+	cout << "---- ISEMPTY ----" << endl;
+	bool isempty = myStack.IsEmpty();
+	cout << "Used: " << myStack.Size() << endl;
+	cout << "Values are:" << endl;
+	
+	if (isempty)
+	{
+		cout << "true" << endl;
+	}
+	else
+	{
+		cout << "false" << endl;
+	}
+	cout << "---- ISEMPTY ----" << endl << endl;
+
+	cout << "---- PUSH ----" << endl;
+	myStack.Push(4);
+	myStack.Push(5);
+	myStack.Push(3);
+	cout << "Used: " << myStack.Size() << endl;
+	cout << "Values are:" << endl;
+	cout << myStack.Top() << endl;
+	cout << "---- PUSH ----" << endl << endl;
+
+	cout << "---- POP ----" << endl;
+	myStack.Pop();
+	cout << "Used: " << myStack.Size() << endl;
+	cout << "Values are:" << endl;
+	cout << myStack.Top() << endl;
+	cout << "---- POP ----" << endl << endl;
+
+	cout << "---- SECONDLAST ----" << endl;
+	myStack.SecondLast();
+	cout << "Used: " << myStack.Size() << endl;
+	cout << "Values are:" << endl;
+	cout << myStack.SecondLast() << endl;
+	cout << "---- SECONDLAST ----" << endl << endl;
+	
+	cout << "//////// TEST STACK ////////" << endl << endl << endl << endl;
 
 	system("Pause");
 	return 0;
